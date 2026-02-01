7a7c4fa17bb12eaf1549f550268b57e09daffa91
aidah <aidah@aidahs-MacBook-Air.local>
2026-02-01 12:19:02 +0530

    Update portfolio layout and add dark mode

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-function sayHello() {
-  document.getElementById("message").innerText =
-    "Thanks for visiting my portfolio!";
-    function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({
-    behavior: "smooth"
-  });
-}
-
-function sayHello() {
-  document.getElementById("message").innerText =
-    "Thanks for visiting my portfolio!";
-}
-}
+// Dark Mode Toggle
 const toggleBtn = document.getElementById("themeToggle");
 
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-
-  if (document.body.classList.contains("dark-mode")) {
+if (toggleBtn) {
+  // Load saved theme
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
     toggleBtn.innerText = "☀️ Light Mode";
-  } else {
-    toggleBtn.innerText = "🌙 Dark Mode";
   }
-});
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+      toggleBtn.innerText = "☀️ Light Mode";
+      localStorage.setItem("theme", "dark");
+    } else {
+      toggleBtn.innerText = "🌙 Dark Mode";
+      localStorage.setItem("theme", "light");
+    }
+  });
+}
